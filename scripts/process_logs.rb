bafa3ab92fe2236ff396c22e007973306ff0eb96
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-08-26 12:06:40 +0200

    orogen module, which combines sensor data into an environment

---
+++ scripts/process_logs.rb
@@ -0,0 +1,63 @@
+#! /usr/bin/env ruby
+
+require 'vizkit'
+require 'asguard'
+require 'optparse'
+include Orocos
+
+def usage
+    STDERR.puts 
+    exit 1
+end
+
+out_file = "/tmp/env"
+
+opt_parse = OptionParser.new do |opt|
+    opt.banner = "process_logs.rb <log_file_dir>"
+    opt.on("-o output_environment", String, "path to output environment") do |name|
+	out_file = name
+    end
+end
+
+args = opt_parse.parse( ARGV )
+if args.size < 1
+    puts opt_parse
+    exit 1
+end
+
+log_file = args[0]
+# filter out properties.log files
+replay = Asguard::Replay.new( log_file )
+
+Orocos.initialize
+Orocos::Process.spawn 'envire_test', 'valgrind'=>false, "wait" => 1000 do |p|
+    envire = p.task('envire')
+
+    envire.environment_debug_path = out_file
+
+    replay.log.hokuyo.scans.connect_to( envire.scan_samples, :type => :buffer, :size => 1000 ) 
+    replay.log.odometry.bodystate_samples.connect_to( envire.bodystate_samples, :type => :buffer, :size => 1000 )
+    replay.log.odometry.odometry_samples.connect_to( envire.dynamic_transformations, :type => :buffer, :size => 1000 ) do |data|
+	yaw = data.orientation.to_euler(2,1,0)[0]
+	data.orientation = Eigen::Quaternion.from_angle_axis( -yaw, Eigen::Vector3.UnitZ ) *
+	    data.orientation 
+	data.position[0] = 0
+	data.position[1] = 0
+	data
+    end
+    replay.log.dynamixel.lowerDynamixel2UpperDynamixel.connect_to( envire.dynamic_transformations, :type => :buffer, :size => 1000 )
+    #if @log_replay.has_task? :dense_stereo and @log_replay.dense_stereo.has_port? :distance_frame
+    #    @log_replay.dense_stereo.distance_frame.connect_to( @eslam.distance_frames, :type => :buffer, :size => 2 )
+    #    @has_distance_images = true
+    #    puts "INFO: Using distance images."
+    #end
+
+    tf = Asguard::Transform.new [:dynamixel]
+    tf.setup_filters replay
+
+    envire.configure
+    envire.start
+    tf.configure_task envire
+
+    replay.log.run
+end
---
+++ tasks/Task.cpp
@@ -0,0 +1,214 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "Task.hpp"
+#include <envire/Core.hpp>
+#include <envire/maps/Pointcloud.hpp>
+#include <envire/maps/LaserScan.hpp>
+#include <envire/operators/ScanMeshing.hpp>
+
+#include <base/angle.h>
+#include <algorithm>
+
+using namespace envire;
+
+Task::Task(std::string const& name, TaskCore::TaskState initial_state)
+    : TaskBase(name, initial_state), env( new Environment() )
+{
+}
+
+Task::Task(std::string const& name, RTT::ExecutionEngine* engine, TaskCore::TaskState initial_state)
+    : TaskBase(name, engine, initial_state), env( new Environment() )
+{
+}
+
+Task::~Task()
+{
+}
+
+void Task::bodystate_samplesTransformerCallback(const base::Time &ts, const ::asguard::BodyState &bodystate_samples_sample)
+{
+    bodyState = bodystate_samples_sample;
+}
+
+void Task::distance_framesTransformerCallback(const base::Time &ts, const ::dense_stereo::distance_image &distance_frames_sample)
+{
+    Eigen::Affine3d body2odometry, lcamera2body;
+    if( !_body2odometry.get( ts, body2odometry ) || !_lcamera2body.get( ts, lcamera2body ) )
+	return;
+}
+
+struct Statistics
+{
+    double S_x;
+    double S_xx;
+    size_t n;
+    mutable std::vector<double> values;
+
+    Statistics() : S_x(0), S_xx(0), n(0) {};
+    void addValue( double v ) { S_x += v; S_xx += pow(v,2); n++; values.push_back( v ); }
+    double getMean() const { return S_x / n; }
+    double getStdev() const { return sqrt( S_xx / n - pow( S_x / n, 2 ) ); }
+    double getMedian() const 
+    { 
+	std::sort( values.begin(), values.end() ); 
+	return values[values.size()/2]; 
+    }
+};
+
+inline std::ostream& operator << (std::ostream& io, Statistics const& stat)
+{
+    io << " samples: " << stat.n
+	<< " mean: " << stat.getMean() 
+	<< " stdev: " << stat.getStdev() 
+	<< " median: " << (stat.n > 0 ? stat.getMedian() : 0);
+    return io;
+}
+
+struct LaserBeam
+{
+    Eigen::Affine3d laser2odometry;
+    Eigen::Vector3d point;
+};
+
+std::vector<LaserBeam> beams;
+Statistics bodyStat;
+Statistics marker1, marker2;
+
+void Task::scan_samplesTransformerCallback(const base::Time &ts, const ::base::samples::LaserScan &scan_samples_sample)
+{
+    Eigen::Affine3d body2odometry, laser2body;
+    if( !_body2odometry.get( ts, body2odometry ) || !_laser2body.get( ts, laser2body ) )
+	return;
+
+    // get all the contact points for the body and add them to pointcloud
+    FrameNode* bodyFrame = new FrameNode( body2odometry );
+    env->addChild( env->getRootNode(), bodyFrame );
+
+    Pointcloud *contactPc = new Pointcloud();
+    env->setFrameNode( contactPc, bodyFrame );
+
+    asguard::Configuration asguardConfig( _asguard_config.get() );
+
+    for(int i=0;i<4;i++)
+    {
+	for(int j=0;j<5;j++) 
+	{
+	    base::Vector3d f = asguardConfig.getFootPosition( bodyState, static_cast<asguard::wheelIdx>(i), j );
+	    contactPc->vertices.push_back( f );
+
+	    if( f.z() < 0 )
+	    {
+		bodyStat.addValue( (body2odometry * f).z() );
+	    }
+	}
+    }
+
+    // get the laserscan and set up the operator chain
+    FrameNode* laserFrame = new FrameNode( laser2body );
+    env->addChild( bodyFrame, laserFrame );
+
+    LaserScan *scanNode = new LaserScan();
+    scanNode->addScanLine( 0, scan_samples_sample );
+    env->setFrameNode( scanNode, laserFrame );
+
+    TriMesh *laserPc = new TriMesh();
+    env->setFrameNode( laserPc, laserFrame );
+
+    ScanMeshing *smOp = new ScanMeshing();
+    env->attachItem( smOp );
+    smOp->addInput( scanNode );
+    smOp->addOutput( laserPc );
+
+    smOp->updateAll();
+
+    if( laserPc->vertices.size() > 500 )
+    {
+	LaserBeam beam;
+	beam.laser2odometry = body2odometry * laser2body;
+	beam.point = laserPc->vertices[460];
+	beam.point = laserPc->vertices[560];
+
+	beams.push_back( beam );
+
+	for( int i=0; i<100 ; i++ )
+	{
+	    Eigen::Vector3d p = beam.laser2odometry * laserPc->vertices[1080/2-50+i];
+	    if( p.z() > 0.1 )
+	    {
+		if( p.y() > 0.7 )
+		    marker1.addValue( p.y() );
+		else
+		    marker2.addValue( p.y() );
+	    }
+	}
+    }
+
+}
+
+void Task::updateHook()
+{
+    TaskBase::updateHook();
+
+    // only write output if the aggregator actually had some data
+    // this is slightly implicit, and could be made more explicit in
+    // the aggregator
+    if( transformer.getStatus().latest_time > base::Time() )
+	_streamaligner_status.write( transformer.getStatus() );
+}
+
+void Task::stopHook()
+{
+    std::cout << "bodyStat: " << bodyStat << std::endl;
+    std::cout << "marker1: " << marker1 << std::endl;
+    std::cout << "marker2: " << marker2 << std::endl;
+
+    // write environment, if path is given
+    if( !_environment_debug_path.value().empty() )
+    {
+	envire::Serialization so;
+	so.serialize(env.get(), _environment_debug_path.value() );
+    }
+
+    double min_stdev = 100;
+    double min_mean = 0;
+    double min_y = 0;
+    double min_z = 0;
+    double min_alpha = 0;
+    double min_median = 0;
+
+    for( float alpha = base::Angle::deg2Rad(-5); alpha < base::Angle::deg2Rad(5); alpha += base::Angle::deg2Rad(.1) )
+    {
+	std::cerr << ".";
+	for( float y = -0.0; y <= 0.0; y += 0.005 )
+	{
+	    for( float z = -0.0; z <= 0.0; z += 0.005 )
+	    {
+		Statistics s;
+		for( size_t i=0; i<beams.size(); i++ )
+		{
+		    s.addValue( (beams[i].laser2odometry * Eigen::AngleAxisd( alpha, Eigen::Vector3d::UnitX() ) * Eigen::Translation3d( Eigen::Vector3d( 0, y, z ) ) * beams[i].point ).z() );
+		}
+
+		if( s.getStdev() < min_stdev )
+		{
+		    min_alpha = alpha;
+		    min_y = y;
+		    min_z = z;
+		    min_stdev = s.getStdev();
+		    min_mean = s.getMean();
+		    min_median = s.getMedian();
+		}
+	    }
+	}
+    }
+
+    std::cout 
+	<< " min_mean: " << min_mean
+	<< " min_stdev: " << min_stdev 
+	<< " min_median: " << min_median 
+	<< " min_alpha: " << min_alpha
+	<< " min_y: " << min_y
+	<< " min_z: " << min_z
+	<< std::endl;
+}
+
